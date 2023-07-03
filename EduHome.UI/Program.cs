e5927442098d47380bba45376b424bd68b31f47e
huseynovth <tahireh@code.edu.az>
2023-07-03 17:58:45 +0400

    Slider back to  UI

--- EduHome.UI/Program.cs
+++ EduHome.UI/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 });
 var app = builder.Build();
 
-
+app.UseStaticFiles();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
